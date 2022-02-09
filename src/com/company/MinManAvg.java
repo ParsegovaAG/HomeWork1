e6ac071174567a9d5a0a12eee982bfa02a0f89c4
ParsegovaAG <alina.parsegova@gmail.com>
2022-02-09 21:05:11 +0300

    Home Work 1. QA

--- src/com/company/MinManAvg.java
+++ src/com/company/MinManAvg.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 
 public class MinManAvg {
 
+    public static int n = 5;
+
+
+    public static double[] createRandomArray(int n) {
+        double[] RandomArray = new double[n];
+        for(int i = 0; i < RandomArray.length; i++){
+            RandomArray[i] = Math.random();
+        }
+        return RandomArray;
+    }
+
     public static void getMinManAvg(double[] arr) {
         // WRITE CODE HERE
 
@@ -13,9 +24,9 @@ public class MinManAvg {
     }
 
     public static void main(String[] args) {
-        RandomArray random_array = new RandomArray();
+
         System.out.println("Печатаем среднее, максимальное и минимальные значения массива");
-        getMinManAvg(random_array.createRandomArray(random_array.n));
+        getMinManAvg(createRandomArray(n));
         System.out.println();
     }
 }
